c3bbf418a13ec58adfa301af6e84b474d24d885e
liukefu <liukefu2020@sina.com>
2021-07-30 17:28:05 +0800

    share demo

--- android/app/src/main/java/com/xiushangapp/wxapi/WXEntryActivity.java
+++ android/app/src/main/java/com/xiushangapp/wxapi/WXEntryActivity.java
@@ -1,6 +1,5 @@
 package com.xiushangapp.wxapi;
 
-import com.mgUmeng.module.wxapi.WXEntryUShareActivity;
 import com.umeng.socialize.weixin.view.WXCallbackActivity;
 
 public class WXEntryActivity extends WXCallbackActivity {
