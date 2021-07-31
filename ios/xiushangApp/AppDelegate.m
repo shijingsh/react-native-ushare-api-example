ce775b7615d74f48ed711cad6cd01873cbc2a7d1
liukefu <liukefu2020@sina.com>
2021-07-31 12:53:31 +0800

    ios集成测试

--- ios/xiushangApp/AppDelegate.m
+++ ios/xiushangApp/AppDelegate.m
@@ -3,6 +3,10 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <UMShare/UMShare.h>
+#import <UMShare/UMSocialManager.h>
+#import <UMShare/WXApi.h>
+#import <UMCommon/UMCommon.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -47,6 +51,29 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  
+  /* Umeng init */
+  [UMConfigure initWithAppkey:@"5fe93fab44bb94418a66cf41" channel:@"App Store" ];
+
+  //配置微信平台的Universal Links
+  //微信和QQ完整版会校验合法的universalLink，不设置会在初始化平台失败
+  [UMSocialGlobal shareInstance].universalLinkDic = @{@(UMSocialPlatformType_WechatSession):@"https://www.xiushangsh.com/xiushangApp/",
+                                                      @(UMSocialPlatformType_QQ):@"https://www.xiushangsh.com/xiushangApp/"
+                                                      };
+  /* Share init */
+
+
+  /* 微信聊天 */
+  [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_WechatSession appKey:@"wx37508906e9cb2cda" appSecret:@"e53dcbd6ab70402b757a59d4b7a10c83" redirectURL:nil];
+
+  /*QQ*/
+  [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_QQ appKey:@"1106152098"  appSecret:@"KexLaOiU6VVzxgQb" redirectURL:nil];
+
+  /* 新浪 */
+  [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_Sina appKey:@"652858587"  appSecret:@"fd9b6e1035d2a865e624257cfebce847" redirectURL:nil];
+
+  
   return YES;
 }
 
@@ -57,6 +84,25 @@ static void InitializeFlipper(UIApplication *application) {
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+
+//设置系统回调
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    BOOL result = [[UMSocialManager defaultManager] handleOpenURL:url];
+    if (!result) {
+        // 其他如支付等SDK的回调
+    }
+    return result;
+}
+//UniversalLink 回调
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void(^)(NSArray * __nullable restorableObjects))restorationHandler
+{
+    if (![[UMSocialManager defaultManager] handleUniversalLink:userActivity options:nil]) {
+        // 其他SDK的回调
+    }
+    return YES;
 }
 
 @end
